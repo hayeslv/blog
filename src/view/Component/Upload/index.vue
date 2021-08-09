20513082c82cc09674bb12f0a7618d7d91e6383a
lvhaizhou <lvhaizhou2012@gmail.com>
2021-08-09 11:46:52 +0800

    自动上传

--- src/view/Component/Upload/index.vue
+++ src/view/Component/Upload/index.vue
@@ -1,20 +1,34 @@
 <template>
   <div>
-    <h2>Element-UI 拖拽上传</h2>
-    <FileUploadElui></FileUploadElui>
-    <h2>Element-UI 文件缩略图</h2>
+    <div class="block">
+      <h2>Element-UI 拖拽上传</h2>
+      <FileUploadEluiDrag></FileUploadEluiDrag>
+    </div>
+    <div class="liner"></div>
+    <div class="block">
+      <h2>Element-UI 文件缩略图（设置只接受图片、自动上传）</h2>
+      <FileUploadEluiImage></FileUploadEluiImage>
+    </div>
   </div>
 </template>
 
 <script>
-import FileUploadElui from './FileUploadElui';
+import FileUploadEluiDrag from './FileUploadEluiDrag';
+import FileUploadEluiImage from './FileUploadEluiImage';
 export default {
-  components: { FileUploadElui }
+  components: { FileUploadEluiDrag, FileUploadEluiImage }
 };
 </script>
 
 <style lang="scss" scoped>
 h2{
   font-weight: bold;
+  margin-bottom: 5px;
+}
+.liner{
+  width: 100%;
+  height: 2px;
+  background-color: #bbb;
+  margin: 30px 0;
 }
 </style>
