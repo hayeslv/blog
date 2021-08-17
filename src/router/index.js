3899c0d43983c58940f38c5b7457bf148a0a840b
lvhaizhou <lvhaizhou2012@gmail.com>
2021-08-17 11:28:16 +0800

    husky

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,8 @@
+/*
+ * @Author: Lvhz
+ * @Date: 2021-08-17 10:32:13
+ * @Description: Description
+ */
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
@@ -10,11 +15,7 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue")
   },
 ];
 
