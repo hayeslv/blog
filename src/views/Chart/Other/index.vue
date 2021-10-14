808bfe81c47a614c9da3b0e476996434a8161dc9
lvhaizhou <417703682@qq.com>
2021-10-14 10:29:20 +0800

    柱状图+折线图

--- src/views/Chart/Other/index.vue
+++ src/views/Chart/Other/index.vue
@@ -8,6 +8,7 @@
     <Other1 class="echart-panel"></Other1>
     <Other2 class="echart-panel"></Other2>
     <Other3 class="echart-panel"></Other3>
+    <Other4 class="echart-panel"></Other4>
   </div>
 </template>
 
@@ -15,8 +16,9 @@
 import Other1 from './Other1';
 import Other2 from './Other2';
 import Other3 from './Other3';
+import Other4 from './Other4';
 export default {
-  components: { Other1, Other2, Other3 }
+  components: { Other1, Other2, Other3, Other4 }
 };
 </script>
 
