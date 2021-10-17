e3542955694a30ae17c2b37e1b43ea8d56c18dd0
lvhaizhou <417703682@qq.com>
2021-10-17 17:23:11 +0800

    editor 类型

--- src/components/drag-drop/types/editor.types.ts
+++ src/components/drag-drop/types/editor.types.ts
@@ -3,3 +3,65 @@
  * @Date: 2021-10-17 16:57:18
  * @Description: Description
  */
+import DragNode from '../object/DragNode'
+
+export type ComponentDesc = {
+  type : string,
+  x : number,
+  y : number
+}
+
+export type DragPageState = {
+  list : Array<ComponentDesc>
+}
+
+export enum States {
+  State,
+  DragState,
+  Moving,
+  Stoped,
+  Selected,
+  PlacingComponent,
+  AddingComponent
+}
+
+export enum Actions {
+  AUTO,
+  EvtDragStart,
+  EvtDrag,
+  EvtDrop,
+  EvtDragEnd,
+  StartAddComponent
+}
+
+export type DragData = {
+  dragging : boolean,
+  startX : number,
+  startY : number,
+  x : number,
+  y : number,
+  diffX : number,
+  diffY : number
+}
+
+export type RawDragEvents = {
+  onDragstart ? : (e : DragNode) => void,
+  onDragend ? : (e : DragNode) => void,
+  onDrag ? : (e : DragNode) => void
+}
+
+export type DragEvents = {
+  onDragstart ? : (e : DragNode) => void,
+  onDragend ? : (e : DragNode) => void,
+  onDrag ? : (e : DragNode) => void
+}
+
+export type DraggableProps = {
+  initialPosition : [number, number]
+} & DragEvents
+
+export type Meta = {
+  type : string,
+  w : number,
+  h : number
+}
