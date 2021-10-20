7482b6cf415052cdc274c0e70c42ecda62ddbbe4
lvhaizhou <417703682@qq.com>
2021-10-20 10:40:10 +0800

    漏斗图

--- src/views/Chart/Other/index.vue
+++ src/views/Chart/Other/index.vue
@@ -10,6 +10,7 @@
     <Other3 class="echart-panel"></Other3>
     <Other4 class="echart-panel"></Other4>
     <Other5 class="echart-panel"></Other5>
+    <Other6 class="echart-panel"></Other6>
   </div>
 </template>
 
@@ -19,8 +20,9 @@ import Other2 from './Other2';
 import Other3 from './Other3';
 import Other4 from './Other4';
 import Other5 from './Other5';
+import Other6 from './Other6';
 export default {
-  components: { Other1, Other2, Other3, Other4, Other5 }
+  components: { Other1, Other2, Other3, Other4, Other5, Other6 }
 };
 </script>
 
