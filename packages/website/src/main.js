8aec676e1d89bc616267fe36ef881da00a4f4439
lvhaizhou <417703682@qq.com>
2021-10-25 15:58:48 +0800

    公共样式

--- packages/website/src/main.js
+++ packages/website/src/main.js
@@ -1,5 +1,12 @@
+/*
+ * @Author: Lvhz
+ * @Date: 2021-09-23 15:30:15
+ * @Description: Description
+ */
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+import "./assets/styles/common.scss";
 
 createApp(App).use(router).mount("#app");
--- packages/website/src/views/About.vue
+++ packages/website/src/views/About.vue
@@ -4,18 +4,31 @@
  * @Description: Description
 -->
 <template>
-  <div v-html="mdText"></div>
+  <div v-html="code"></div>
 </template>
 
 <script>
 import { ref } from "vue";
 import marked from "marked";
-import testMd from "../docs/plugin-use/marked.md";
+// import testMd from "../docs/plugin-use/marked.md";
+import testMd from "../docs/button.md";
+
+// marked.setOptions({
+//   highlight: function (code, lang, callback) {
+//     require("pygmentize-bundled")(
+//       { lang: lang, format: "html" },
+//       code,
+//       function (err, result) {
+//         callback(err, result.toString());
+//       }
+//     );
+//   },
+// });
 
 export default {
   setup() {
-    const mdText = ref(marked(testMd));
-    return { mdText };
+    const code = ref(marked(testMd));
+    return { code };
   },
 };
 </script>
