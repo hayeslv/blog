7d16044b2d4bd814c98499cd391dba6771e868c8
lvhaizhou <417703682@qq.com>
2021-10-28 15:36:06 +0800

    change

--- packages/website/src/router/index.js
+++ packages/website/src/router/index.js
@@ -3,83 +3,8 @@
  * @Date: 2021-09-23 15:30:15
  * @Description: Description
  */
-// import { defineAsyncComponent } from "vue";
-// import { createRouter, createWebHistory } from "vue-router";
-// import navConfig from "./nav.config";
-
-// const load = (path) =>
-//   defineAsyncComponent(() => import(`../pages/${path}.vue`));
-
-// const loadDocs = (path) =>
-//   defineAsyncComponent(() => import(`../docs${path}.md`));
-
-// // 注册路由
-// function registerRoute(navConfig) {
-//   const route = [];
-
-//   // 组件页第一个页面
-//   route.push({
-//     path: `/component`,
-//     redirect: `/component/quickstart`,
-//     component: load("component"),
-//     children: [],
-//   });
-//   navConfig.forEach((nav) => {
-//     if (nav.href) return;
-//     if (nav.groups) {
-//       nav.groups.forEach((group) => {
-//         group.list.forEach((nav) => {
-//           addRoute(nav);
-//         });
-//       });
-//     } else if (nav.children) {
-//       nav.children.forEach((nav) => {
-//         addRoute(nav);
-//       });
-//     } else {
-//       addRoute(nav);
-//     }
-//   });
-
-//   function addRoute(page) {
-//     const component = loadDocs(page.path);
-//     const child = {
-//       path: page.path.slice(1),
-//       meta: {
-//         title: page.title || page.name,
-//         description: page.description,
-//       },
-//       name: "component" + (page.title || page.name),
-//       component: component.default || component,
-//     };
-
-//     route[0].children.push(child);
-//   }
-
-//   return route;
-// }
-
-// let routes = registerRoute(navConfig);
-
-// // 静态路由
-// const staticRoutes = [
-//   {
-//     path: "/", // 首页
-//     name: "home",
-//     component: load("index"),
-//   },
-// ];
-
-// routes = routes.concat(staticRoutes);
-
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes,
-// });
-
-// export default router;
-
 import { defineAsyncComponent } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import navConfig from "./nav.config";
 
 const load = (path) =>
@@ -88,16 +13,21 @@ const load = (path) =>
 const loadDocs = (path) =>
   defineAsyncComponent(() => import(`../docs${path}.md`));
 
-const registerRoute = (navConfig) => {
+// 注册路由
+function registerRoute(navConfig) {
   const route = [];
 
+  // 组件页第一个页面
   route.push({
     path: `/component`,
     redirect: `/component/quickstart`,
     component: load("component"),
     children: [],
   });
-
+  route.push({
+    path: `/about`,
+    component: () => import("../views/About"),
+  });
   navConfig.forEach((nav) => {
     if (nav.href) return;
     if (nav.groups) {
@@ -116,8 +46,7 @@ const registerRoute = (navConfig) => {
   });
 
   function addRoute(page) {
-    const component =
-      page.path === "/changelog" ? load("changelog") : loadDocs(page.path);
+    const component = loadDocs(page.path);
     const child = {
       path: page.path.slice(1),
       meta: {
@@ -132,26 +61,101 @@ const registerRoute = (navConfig) => {
   }
 
   return route;
-};
+}
 
-let route = registerRoute(navConfig);
+let routes = registerRoute(navConfig);
 
-const generateRoutes = function () {
-  const resourceRoute = {
-    path: `/resource`, // 资源
-    name: "resource",
-    component: load("resource"),
-  };
-
-  const indexRoute = {
-    path: `/`, // 首页
+// 静态路由
+const staticRoutes = [
+  {
+    path: "/", // 首页
     name: "home",
     component: load("index"),
-  };
+  },
+];
 
-  return [resourceRoute, indexRoute];
-};
+routes = routes.concat(staticRoutes);
 
-route = route.concat(generateRoutes());
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default route;
+export default router;
+
+// import { defineAsyncComponent } from "vue";
+// import navConfig from "./nav.config";
+
+// const load = (path) =>
+//   defineAsyncComponent(() => import(`../pages/${path}.vue`));
+
+// const loadDocs = (path) =>
+//   defineAsyncComponent(() => import(`../docs${path}.md`));
+
+// const registerRoute = (navConfig) => {
+//   const route = [];
+
+//   route.push({
+//     path: `/component`,
+//     redirect: `/component/quickstart`,
+//     component: load("component"),
+//     children: [],
+//   });
+
+//   navConfig.forEach((nav) => {
+//     if (nav.href) return;
+//     if (nav.groups) {
+//       nav.groups.forEach((group) => {
+//         group.list.forEach((nav) => {
+//           addRoute(nav);
+//         });
+//       });
+//     } else if (nav.children) {
+//       nav.children.forEach((nav) => {
+//         addRoute(nav);
+//       });
+//     } else {
+//       addRoute(nav);
+//     }
+//   });
+
+//   function addRoute(page) {
+//     const component =
+//       page.path === "/changelog" ? load("changelog") : loadDocs(page.path);
+//     const child = {
+//       path: page.path.slice(1),
+//       meta: {
+//         title: page.title || page.name,
+//         description: page.description,
+//       },
+//       name: "component" + (page.title || page.name),
+//       component: component.default || component,
+//     };
+
+//     route[0].children.push(child);
+//   }
+
+//   return route;
+// };
+
+// let route = registerRoute(navConfig);
+
+// const generateRoutes = function () {
+//   const resourceRoute = {
+//     path: `/resource`, // 资源
+//     name: "resource",
+//     component: load("resource"),
+//   };
+
+//   const indexRoute = {
+//     path: `/`, // 首页
+//     name: "home",
+//     component: load("index"),
+//   };
+
+//   return [resourceRoute, indexRoute];
+// };
+
+// route = route.concat(generateRoutes());
+
+// export default route;
--- packages/website/vue.config.js
+++ packages/website/vue.config.js
@@ -4,22 +4,22 @@
  * @Date: 2021-10-14 11:14:46
  * @Description: Description
  */
-const path = require('path')
+// const path = require('path')
 module.exports = {
   devServer: {
-    port: 9888,
+    port: 7777,
   },
   chainWebpack: (config) => {
     // 添加解析md的loader
-    // config.module
-    //   .rule('md')
-    //   .test(/\.md$/)
-    //   .use('html-loader')
-    //   .loader('html-loader')
-    //   .end()
-    //   .use('markdown-loader')
-    //   .loader('markdown-loader')
-    //   .end()
+    config.module
+      .rule('md')
+      .test(/\.md$/)
+      .use('html-loader')
+      .loader('html-loader')
+      .end()
+      .use('markdown-loader')
+      .loader('markdown-loader')
+      .end()
 
     // config
     //   // app entry
@@ -28,15 +28,15 @@ module.exports = {
     //   .add(path.resolve(__dirname, './src/main.js'))
     //   .end()
 
-    // 添加解析 md 的 loader
-    config.module
-      .rule('md2vue')
-      .test(/\.md$/)
-      .use('vue-loader')
-      .loader('vue-loader')
-      .end()
-      .use('md-loader')
-      .loader(path.resolve(__dirname, '../md-loader/src/index.js'))
-      .end()
+    // // 添加解析 md 的 loader
+    // config.module
+    //   .rule('md2vue')
+    //   .test(/\.md$/)
+    //   .use('vue-loader')
+    //   .loader('vue-loader')
+    //   .end()
+    //   .use('md-loader')
+    //   .loader(path.resolve(__dirname, '../md-loader/src/index.js'))
+    //   .end()
   },
 };
