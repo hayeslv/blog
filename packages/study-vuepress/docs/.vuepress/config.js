561f15b1ef28246822eb9dffdf94199e5c39845c
lvhaizhou <417703682@qq.com>
2021-10-29 20:05:28 +0800

    vuepress配置

--- packages/study-vuepress/docs/.vuepress/config.js
+++ packages/study-vuepress/docs/.vuepress/config.js
@@ -5,21 +5,64 @@
  */
 module.exports = {
   title: 'dylan\'s blog',
-  description: '个人网站',
-  head: [ // 注入到当前页面的 HTML <head> 中的标签
-    // ['link', { rel: 'icon', href: '../' }] // 增加一个自定义的 favicon(网页标签的图标)
-  ],
-  base: '/', // 这是部署到github相关的配置
+  description: '组件文档',
+  // head: [ // 注入到当前页面的 HTML <head> 中的标签
+  //   // ['link', { rel: 'icon', href: '../' }] // 增加一个自定义的 favicon(网页标签的图标)
+  // ],
+  // base: '/', // 这是部署到github相关的配置
   markdown: {
     lineNumbers: false // 代码块显示行号
   },
+  // actionText: '123123',
   themeConfig: {
     nav:[ // 导航栏配置
-      {text: '前端基础', link: '/accumulate/' },
-      {text: '算法题库', link: '/algorithm/'},
-      {text: '微博', link: 'https://baidu.com'}      
+      {text: '算法', link: '/algorithm/base-栈' },
+      // 显示下拉列表
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Chinese', link: '/language/chinese' },
+          { text: 'Japanese', link: '/language/japanese' }
+        ]
+      },
+      // 下拉列表显示分组
+      {
+        text: '高级',
+        items: [
+          { 
+            text: '算法', 
+            items: [
+              { text: '冒泡', link: '/language/chinese' },
+              { text: '快速', link: '/language/japanese' }
+            ] 
+          },
+          { 
+            text: '设计模式', 
+            items: [
+              { text: '工厂', link: '/language/chinese' },
+              { text: '单例', link: '/language/chinese'},
+            ] 
+          },
+        ]
+      }
     ],
-    sidebar: 'auto', // 侧边栏配置
-    sidebarDepth: 2, // 侧边栏显示2级
+    // sidebar: 'auto', // 侧边栏配置
+    sidebar: [
+      {
+        title: '数据结构',
+        collapsable: false,
+        children: [
+          '/algorithm/base-栈'
+        ]
+      },
+      {
+        title: '插件使用',
+        collapsable: false,
+        children: [
+          '/pluginUse/a'
+        ]
+      }
+    ],
+    sidebarDepth: 0, // 侧边栏显示2级
   }
 }
--- packages/study-vuepress/docs/README.md
+++ packages/study-vuepress/docs/README.md
@@ -1,34 +1,18 @@
-# Hello VuePress
-
-```js
-| Tables        | Are           | Cool  |
-| ------------- |:-------------:| -----:|
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      | centered      |   $12 |
-| zebra stripes | are neat      |    $1 |
-```
-
-:tada: :100:
-
-| Tables        |      Are      |  Cool |
-| ------------- | :-----------: | ----: |
-| zebra stripes |   are neat    |    $1 |
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      |   centered    |   $12 |
-
-
-
-::: details 点击查看代码
-
-这是一个提示
-
-```js
-console.log('hahahaha')
-```
-
-
-
-:::
+---
+home: true
+heroImage: 
+actionText: 快速上手 →
+actionLink: /one
+features:
+- title: 简洁至上
+  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+# footer: MIT Licensed | Copyright © 2018-present Evan You
+footer: MIT Licensed | Copyright
+---
 
 
 
