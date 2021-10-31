21825ef5f2ff10702216c0e9682f067c2c9cd785
lvhaizhou <417703682@qq.com>
2021-10-31 20:07:12 +0800

    rxjs

---
+++ src/views/开胃菜/观察者模式/example1.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: Lvhz
+ * @Date: 2021-10-31 19:58:25
+ * @Description: Description
+ */
+const { Observable } = require('rxjs');
+
+(() => {
+  const observable = new Observable((observer) => {
+    let i = 0;
+    setInterval(() => {
+      observer.next(i++);
+    }, 1000)
+  })
+
+  observable.subscribe((i) => {
+    console.log('tick', i);
+  })
+})()
+
