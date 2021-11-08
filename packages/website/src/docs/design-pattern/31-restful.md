7ba689e85258749987a5099ce46c744200cad223
lvhaizhou <417703682@qq.com>
2021-11-08 10:30:54 +0800

    偏前端的设计模式

---
+++ packages/website/src/docs/design-pattern/31-restful.md
@@ -0,0 +1,9 @@
+# Restful
+
+Restful是一套前端 + 后端协作标准。
+
+- 前端无状态，前端只有表示类的数据（Representation）
+- 服务端有状态
+- 用户通过 transfer 改变服务端状态
+- 用名词性 + HTTP Method 描述 transfer
+
--- packages/website/src/views/test/ui.ts
+++ packages/website/src/views/test/ui.ts
@@ -3,15 +3,4 @@
  * @Date: 2021-11-07 16:44:27
  * @Description: Description
  */
-interface UIInfo {
-  /** @deprecated use box instead **/
-  width : number;
-  height : number;
 
-  box : number;
-}
-
-
-function print(box : UIInfo) {
-  console.log(box.width);
-}
